36f192de72e77b3b718fe65fa336101f06ebf99a
Julien Martin <balteo@gmail.com>
2020-03-27 10:58:16 +0100

    Introduce storybook storysource addon

--- libs/my-lib/.storybook/config.js
+++ libs/my-lib/.storybook/config.js
@@ -1,5 +1,7 @@
 import { configure, addDecorator } from '@storybook/angular';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withStorySource } from '@storybook/addon-storysource';
 
 addDecorator(withKnobs);
+addDecorator(withStorySource);
 configure(require.context('../src/lib', true, /\.stories\.tsx?$/), module);
